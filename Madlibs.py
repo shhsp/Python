45e40a7cf37c83120852556191671b21c005804e
HS <113318038+shhsp@users.noreply.github.com>
2023-01-08 17:10:54 -0800

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+Replacement = "MY CHANNEL"
+
+print("subscribe to" + Replacement)
+print("subscribe to {}".format(Replacement))
+print(f"subscribe to {Replacement}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+person = input("Person: ")
+
+madlib = f"Programming is very {adj}. It is very exciting and {verb1}. Stay hydrated and {verb2}"
+
+print(madlib)
